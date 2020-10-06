211c319cafcaf89703c4887a51fc0be77c5d855c
ioni scheines <jonatan.scheines@retargetly.com>
2020-10-06 12:11:48 -0300

    console log port

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,22 +1,23 @@
-import React from "react";
-import io from "socket.io-client";
+import React from 'react';
+import io from 'socket.io-client';
 
-import ConnectScreen from "./screens/Connect";
-import DisconnectedScreen from "./screens/Disconnected";
-import FinishedScreen from "./screens/Finished";
-import LoadingScreen from "./screens/Loading";
-import PlayingScreen from "./screens/Playing";
-import {Game} from "./types";
+import ConnectScreen from './screens/Connect';
+import DisconnectedScreen from './screens/Disconnected';
+import FinishedScreen from './screens/Finished';
+import LoadingScreen from './screens/Loading';
+import PlayingScreen from './screens/Playing';
+import { Game } from './types';
 
-const socket = io(process.env.REACT_APP_SERVER_URL || "/", {
+const socket = io(process.env.REACT_APP_SERVER_URL || '/', {
   autoConnect: false,
 });
 
 function App() {
   const [game, setGame] = React.useState<null | Game>(null);
-  const [status, setStatus] = React.useState<Game["status"]>("init");
+  const [status, setStatus] = React.useState<Game['status']>('init');
 
   function handleConnect(name: string, room: string) {
+    console.log('PUERTO', process.env.REACT_APP_SERVER_URL);
     socket.io.opts.query = {
       name,
       room,
@@ -32,32 +33,34 @@ function App() {
   }
 
   function handleGuess(guess: string) {
-    socket.emit("guess", guess);
+    socket.emit('guess', guess);
   }
 
   React.useEffect(() => {
-    socket.on("connect", () => setStatus("loading"));
-    socket.on("disconnect", () => setStatus("disconnected"));
+    socket.on('connect', () => setStatus('loading'));
+    socket.on('disconnect', () => setStatus('disconnected'));
 
-    socket.on("timeout", () => console.log("TIMEOUT"));
+    socket.on('timeout', () => console.log('TIMEOUT'));
 
-    socket.on("game", onGame);
+    socket.on('game', onGame);
   }, []);
 
   return (
     <main>
-      {status === "init" && <ConnectScreen onConnect={handleConnect} />}
-      {status === "disconnected" && <DisconnectedScreen />}
-      {status === "loading" && <LoadingScreen />}
+      {status === 'init' && <ConnectScreen onConnect={handleConnect} />}
+      {status === 'disconnected' && <DisconnectedScreen />}
+      {status === 'loading' && <LoadingScreen />}
       {Boolean(game?.players?.length) && (
         <ul className="nes-list is-disc" id="players">
-          {game?.players.map((player) => (
+          {game?.players.map(player => (
             <li key={player.id}>{player.name}</li>
           ))}
         </ul>
       )}
-      {game && status === "playing" && <PlayingScreen song={game.song} onGuess={handleGuess} />}
-      {game && status === "finished" && (
+      {game && status === 'playing' && (
+        <PlayingScreen song={game.song} onGuess={handleGuess} />
+      )}
+      {game && status === 'finished' && (
         <FinishedScreen song={game.songTitle} winner={game.winner} />
       )}
     </main>
